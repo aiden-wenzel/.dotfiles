7f47509b7d3db9105a7dd1bbed38cc33ae81d6ae
aidenwen <wenzel.aiden@outlook.com>
2024-04-26 15:07:37 -0400

    .bashrc added

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ git submodule update --init
 
 # remove existing files and configs
 echo "Removing existing files and configs"
-rm -rfv ~/.config/nvim 
+rm -rfv ~/.config/nvim
+rm -rfv ~/.bashrc
 
 # create symlinks to dotfiles
 echo "Create symlinks to dotfiles"
 ln -s /home/$USER/.dotfiles/nvim ~/.config/nvim
+ln -s /home/$USER/.dotfiles/.bashrc ~/.bashrc
