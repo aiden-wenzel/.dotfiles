1dd3c52a7016124a7e709308ac4f8ab425195f53
aidenwen <wenzel.aiden@outlook.com>
2024-04-25 22:18:15 -0400

    added documentation

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-rm -rf ~/.config/nvim 
+# remove existing files and configs
+echo "Removing existing files and configs\n\n"
+rm -rfv ~/.config/nvim 
 
+# add any git modules for existing gitrepos
+echo "Adding github sub modules\n\n"
 git submodule add git@github.com:aiden-wenzel/Neovim_Config.git nvim
 
+# create symlinks to dotfiles
+echo "Create symlinks to dotfiles"
 ln -s /home/$USER/.dotfiles/nvim ~/.config/nvim
