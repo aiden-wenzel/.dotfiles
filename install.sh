0a56a1cb85011a05be141534c2a6cc6dfbe33dff
aidenwen <wenzel.aiden@outlook.com>
2024-04-26 14:52:23 -0400

    removed submodule command

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 echo "Removing existing files and configs\n\n"
 rm -rfv ~/.config/nvim 
 
-# add any git modules for existing gitrepos
-echo "Adding github sub modules\n\n"
-git submodule add git@github.com:aiden-wenzel/Neovim_Config.git nvim
-
 # create symlinks to dotfiles
 echo "Create symlinks to dotfiles"
 ln -s /home/$USER/.dotfiles/nvim ~/.config/nvim
