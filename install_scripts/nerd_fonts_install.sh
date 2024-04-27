cf8340e2be47b240f0297a3577c1e659101be1b7
aidenwen <wenzel.aiden@outlook.com>
2024-04-27 16:15:39 -0400

    removed nerd font install

--- install_scripts/nerd_fonts_install.sh
+++
@@ -1,5 +0,0 @@
-mkdir ~/.fonts
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-mkdir /home/$USER/.fonts/JetBrainsMono
-tar -xf JetBrainsMono.tar.xz -C /home/$USER/.fonts/JetBrainsMono
-rm ./JetBrainsMono.tar.xz
