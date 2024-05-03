7f90976ed16db3f7ee05857099a65912eca06264
aidenwen <wenzel.aiden@outlook.com>
2024-05-02 23:22:40 -0400

    zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # initiate submodules
 git submodule update --init
 
+# set zsh to default shell and install oh my zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # remove existing files and configs
 echo "Removing existing files and configs"
 rm -rfv ~/.config/nvim
@@ -10,3 +14,4 @@ rm -rfv ~/.bashrc
 echo "Create symlinks to dotfiles"
 ln -s /home/$USER/.dotfiles/nvim ~/.config/nvim
 ln -s /home/$USER/.dotfiles/.bashrc ~/.bashrc
+ln -s /home/$USER/.dotfiles/.zshrc ~/.zshrc
