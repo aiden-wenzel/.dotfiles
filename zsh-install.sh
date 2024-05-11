f37ec58d107e14aba9be96ff6ed9f1d16200f872
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-10 21:56:49 -0400

    zsh-install complete

--- zsh-install.sh
+++ zsh-install.sh
@@ -1,4 +1,6 @@
 sudo pacman -S zsh
-chsh -s $(which zsh)
+chsh -s /usr/bin/zsh
 rm -rfv ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+rm -rfv ~/.oh-my-zsh 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
