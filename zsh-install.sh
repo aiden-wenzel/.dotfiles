c76ad93ff0ba3141fbceb7c1c1a7a4bab2eb3971
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-10 22:12:27 -0400

    zsh install update

--- zsh-install.sh
+++ zsh-install.sh
@@ -1,6 +1,9 @@
 sudo pacman -S zsh
-chsh -s /usr/bin/zsh
+rm -rfv ~/.oh-my-zsh
 rm -rfv ~/.zshrc
+chsh -s /usr/bin/zsh
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-rm -rfv ~/.oh-my-zsh 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
