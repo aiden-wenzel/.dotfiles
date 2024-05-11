68434d9ccb9b9e15f228eca754f1e37c0953ccbb
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-10 23:37:04 -0400

    install.sh updates

--- install.sh
+++ install.sh
@@ -1,18 +1,4 @@
-# install packages
-sudo pacman -S cmake 
-
-# clone powerlevel10k for zsh 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# set zsh to default shell and install oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+sudo pacman -Syu 
+sudo pacman -S --needed - < installed_packages.txt
 # make config directory
 mkdir ~/.config
-
-# remove existing files and configs
-echo "Removing existing files and configs"
-
-# create symlinks to dotfiles
-echo "Create symlinks to dotfiles"
-ln -s /home/$USER/.dotfiles/.gitconfig ~/.gitconfig
