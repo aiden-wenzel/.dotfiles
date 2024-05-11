ddc3cc829d080788f1187171350a88bcf2e40ca5
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-11 14:08:40 -0400

    zsh-install.sh fix

--- zsh-install.sh
+++ zsh-install.sh
@@ -2,8 +2,8 @@ sudo pacman -S zsh
 rm -rfv ~/.oh-my-zsh
 rm -rfv ~/.zshrc
 chsh -s /usr/bin/zsh
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+rm -rfv ~/.zshrc ~/.zshrc.pre-oh-my-zsh
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
