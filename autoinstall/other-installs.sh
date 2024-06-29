4693321498716f3d7fdf17b9382004be5f687405
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-06-29 14:50:47 -0400

    test

---
+++ autoinstall/other-installs.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+wget https://nodejs.org/dist/v22.2.0/node-v22.2.0-linux-x64.tar.xz
+sudo tar -xvf node-v22.2.0-linux-x64.tar.xz -C /usr/local
+rm node-v22.2.0-linux-x64.tar.xz
+
+sudo curl -s https://ohmyposh.dev/install.sh | sudo bash -s
--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
+sudo apt update
+sudo apt upgrade
+
+sudo apt install $(cat ./autoinstall/apt-packages.txt)
+sudo snap install $(cat ./autoinstall/snap-packages.txt)
+sh ./autoinstall/other-installs.sh
+
 rm -v ~/.bashrc
 rm -v ~/.gitconfig
 rm -rfv ~/.config/nvim
 rm -v ~/.zshrc
 
-stow .
+stow --ignore="autoinstall" .
 
 git submodule init
 git submodule update
