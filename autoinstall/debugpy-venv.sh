2752d815acf5ebbfa56f899c7aef1e48e9607cd1
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-07-12 22:49:20 -0400

    updated autoinstall

---
+++ autoinstall/debugpy-venv.sh
@@ -0,0 +1,6 @@
+mkdir ~/.virtualenvs
+cd ~/.virtualenvs
+mkdir debugpy
+cd debugpy
+python3 -m venv .venv
+pip install debugpy
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt upgrade
 sudo apt install $(cat ./autoinstall/apt-packages.txt)
 sudo snap install $(cat ./autoinstall/snap-packages.txt)
 sh ./autoinstall/other-installs.sh
+sh ./autoinstall/debugpy-venv.sh
 
 rm -v ~/.bashrc
 rm -v ~/.gitconfig
