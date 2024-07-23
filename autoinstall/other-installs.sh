9b9af2b080652a6e9dd9aab388b0cc7a68317ee4
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-07-22 22:22:55 -0400

    Updates

--- autoinstall/other-installs.sh
+++ autoinstall/other-installs.sh
@@ -4,4 +4,8 @@ wget https://nodejs.org/dist/v22.2.0/node-v22.2.0-linux-x64.tar.xz
 sudo tar -xvf node-v22.2.0-linux-x64.tar.xz -C /usr/local
 rm node-v22.2.0-linux-x64.tar.xz
 
+wget https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb
+sudo dpkg -i git-delta_0.17.0_amd64.deb
+rm git-delta_0.17.0_amd64.deb
+
 sudo curl -s https://ohmyposh.dev/install.sh | sudo bash -s
