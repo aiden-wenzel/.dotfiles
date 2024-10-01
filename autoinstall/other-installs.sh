77d158a05967f657a79022aa1ebc4f0bc131b4f3
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-10-01 15:32:03 -0400

    dotfiles

--- autoinstall/other-installs.sh
+++ autoinstall/other-installs.sh
@@ -7,5 +7,9 @@ rm node-v22.2.0-linux-x64.tar.xz
 wget https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb
 sudo dpkg -i git-delta_0.17.0_amd64.deb
 rm git-delta_0.17.0_amd64.deb
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
 
 sudo curl -s https://ohmyposh.dev/install.sh | sudo bash -s
