16a425bf67f1f44a18e9d75d1142cd876feb4a48
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-01-30 12:43:30 -0500

    forgot this comment

--- autoinstall/other-installs.sh
+++ autoinstall/other-installs.sh
@@ -6,7 +6,7 @@ wget https://nodejs.org/dist/v22.2.0/node-v22.2.0-linux-x64.tar.xz
 sudo tar -xvf node-v22.2.0-linux-x64.tar.xz -C /usr/local
 rm node-v22.2.0-linux-x64.tar.xz
 
-# lazy git 
+# lazy git
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
