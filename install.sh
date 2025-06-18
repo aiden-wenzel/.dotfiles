88fe21cbd5223ada80f5bd6367cadfe5d77c1a24
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-06-17 22:45:04 -0400

    restructure

--- install.sh
+++ install.sh
@@ -5,20 +5,21 @@ sudo apt install $(cat ./autoinstall/apt-packages.txt)
 sh ./autoinstall/other-installs.sh
 sh ./autoinstall/debugpy-venv.sh
 
+# Bashrc
 rm -v ~/.bashrc
-rm -rfv ~/.config/nvim
-rm -v ~/.tmux.conf
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
 
 # Git Config
 rm -v ~/.gitconfig
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # Nvim Installation
+rm -rfv ~/.config/nvim
+git submodule init
+git submodule update
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 
 # Tmux Installation
+rm -v ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-
-git submodule init
-git submodule update
